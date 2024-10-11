938de87e35e760c75d94e61fdd18be15f34d95c0
Dinesh Kottakota <dineshkottakota1234@gmail.com>
2024-10-11 19:59:32 +0530

    Removed API key

--- Web_Development/weather app/app.js
+++ Web_Development/weather app/app.js
@@ -1,4 +1,4 @@
-const apiKey="54a03fbe3798e889a6e7f2f76f317551";
+const apiKey="YOUR_API_KEY";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox=document.querySelector(".search input");
 const searchBtn=document.querySelector(".search button");
